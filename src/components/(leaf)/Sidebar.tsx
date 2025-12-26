e85426e2805ba0ba1bb4ade2db5c986873032bc3
NikhilKatkuri <knikhil07k@gmail.com>
2025-12-26 20:15:45 +0530

    add resizable

--- src/components/(leaf)/Sidebar.tsx
+++ src/components/(leaf)/Sidebar.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
-
 const Sidebar = () => {
-    return <div>Sidebar</div>;
+    return (
+        <div className='h-full w-full bg-(--side-bar-background)'>
+            <div className='flex h-8 w-full items-center bg-(--side-bar-section-header-background) px-2 text-xs font-semibold text-(--side-bar-section-header-foreground)'>
+                <p>Explorer</p>
+            </div>
+        </div>
+    );
 };
 
 export default Sidebar;
--- src/components/(leaf)/StatusBar.tsx
+++ src/components/(leaf)/StatusBar.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const StatusBar = () => {
     return <div>StatusBar</div>;
 };
